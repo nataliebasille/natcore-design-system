77c6fb3740f58529a4c2f634c17acdd559ebc8e1
Natalie Basille <natalie.basille1@gmail.com>
2024-01-01 14:31:47 -0500

    Fix ts error

--- packages/core/src/themes/components/form-control.ts
+++ packages/core/src/themes/components/form-control.ts
@@ -42,16 +42,16 @@ export default (theme: PluginAPI["theme"]) => ({
       paddingTop: ".25em",
 
       [ALL_SIBLING_INPUTS_SELECTOR]: {
-        borderTopLeftRadius: 0,
-        borderTopRightRadius: 0,
+        borderTopLeftRadius: "0",
+        borderTopRightRadius: "0",
         borderTopStyle: "none",
         paddingTop: ".25em",
       },
 
       "~ .form-control-prefix, ~ .form-control-suffix": {
         borderTopStyle: "none",
-        borderTopLeftRadius: 0,
-        borderTopRightRadius: 0,
+        borderTopLeftRadius: "0",
+        borderTopRightRadius: "0",
       },
     },
 
@@ -59,8 +59,8 @@ export default (theme: PluginAPI["theme"]) => ({
       gridArea: "input",
 
       "&:has(~ .form-control-suffix)": {
-        borderTopRightRadius: 0,
-        borderBottomRightRadius: 0,
+        borderTopRightRadius: "0",
+        borderBottomRightRadius: "0",
         borderRight: "none",
         paddingRight: ".25em",
       },
@@ -95,8 +95,8 @@ export default (theme: PluginAPI["theme"]) => ({
       borderBottomLeftRadius: theme("borderRadius.md")!,
       paddingLeft: ".25em",
       [ALL_SIBLING_INPUTS_SELECTOR]: {
-        borderTopLeftRadius: 0,
-        borderBottomLeftRadius: 0,
+        borderTopLeftRadius: "0",
+        borderBottomLeftRadius: "0",
         borderLeft: "none",
         paddingLeft: ".25em",
       },
