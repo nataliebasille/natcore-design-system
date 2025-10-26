0e6706ef607634767ed4dcc465c9aa48f701fd8a
Natalie Basille <natalie.basille1@gmail.com>
2025-10-26 08:38:06 -0400

    transpilePackages in monorepo

--- showcase/next.config.js
+++ showcase/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@natcore/design-system-react"],
+  transpilePackages: ["@natcore/design-system-core", "@natcore/icon", "@natcore/design-system-react"],
   turbopack: {},
   webpack: (config) => {
     config.module.rules.push({
