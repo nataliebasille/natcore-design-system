e5108ed7b89cef9f9008361b4e4e69323ec7f0e4
Natalie Basille <natalie.basille1@gmail.com>
2026-03-22 11:21:22 -0400

    whoops missed a file

--- showcase-v2/app/layout.tsx
+++ showcase-v2/app/layout.tsx
@@ -27,10 +27,10 @@ export default async function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css"
         />
       </head>
-      <body className="bg-tone-50-surface palette-surface grid h-screen w-screen grid-cols-[minmax(16rem,20rem)_minmax(32rem,1fr)] grid-rows-[auto_1fr] overflow-hidden max-md:grid-cols-1">
+      <body className="grid h-screen w-screen grid-cols-[minmax(16rem,20rem)_minmax(32rem,1fr)] grid-rows-[auto_1fr] overflow-hidden bg-tone-50-surface palette-surface max-md:grid-cols-1">
         <ThemeProvider>
-          <Header className="bg-tone-50-surface border-b-tone-600-surface/30 col-span-2 border-b" />
-          <Sidebar className="max-md:tray-left bg-tone-100-surface border-r-tone-600-surface/30 min-h-0 overflow-y-auto border-r max-md:mt-[46px]">
+          <Header className="col-span-2 border-b border-b-tone-600-surface/30 bg-tone-50-surface" />
+          <Sidebar className="min-h-0 overflow-y-auto border-r border-r-tone-600-surface/30 bg-tone-100-surface max-md:tray-left max-md:mt-[46px]">
             <SidebarGroup header="Getting Started">
               <SidebarLink href="/">Introduction</SidebarLink>
               <SidebarLink href="/installation">Installation</SidebarLink>
@@ -49,7 +49,7 @@ export default async function RootLayout({
               <SidebarLink href="/component/tabs">Tabs</SidebarLink>
             </SidebarGroup>
           </Sidebar>
-          <main className="bg-tone-50-surface min-h-0 min-w-0 overflow-x-hidden overflow-y-auto px-4 max-md:col-span-2">
+          <main className="min-h-0 min-w-0 overflow-x-hidden overflow-y-auto bg-tone-50-surface px-4 max-md:col-span-2">
             {children}
           </main>
         </ThemeProvider>
