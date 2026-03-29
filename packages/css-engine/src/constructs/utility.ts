f77099c840097ff10cd76db411e63816d9c18fe2
Natalie Basille <natalie.basille1@gmail.com>
2026-03-29 16:25:02 -0400

    fix build issue

--- packages/css-engine/src/constructs/utility.ts
+++ packages/css-engine/src/constructs/utility.ts
@@ -1,30 +1,17 @@
 import {
   dsl,
   type StyleListAst,
+  type StyleListAst_WithMetadata,
   type StyleListBuilder,
   type StyleRuleAst,
+  type StyleRuleAst_WithMetadata,
   type StyleRuleBodyBuilder,
   type TailwindClassAst,
-  type BodyBuilder_To_StyleRuleBody,
-  type ListBuilder_To_StyleListAst,
+  type TailwindUtility,
 } from "../dsl/public.ts";
 import type { ThemeConstruct } from "./theme.ts";
 
 export type UtilityBodyBuilder = StyleListBuilder | StyleRuleBodyBuilder;
-
-export type BodyBuilder_To_UtilityBody<T extends UtilityBodyBuilder[]> =
-  T extends [infer U, ...infer R extends UtilityBodyBuilder[]] ?
-    [
-      U extends StyleListBuilder ? ListBuilder_To_StyleListAst<[U]>
-      : U extends StyleRuleBodyBuilder ? BodyBuilder_To_StyleRuleBody<[U]>
-      : never,
-      ...BodyBuilder_To_UtilityBody<R>,
-    ]
-  : T extends (infer U)[] ?
-    (U extends StyleListBuilder ? ListBuilder_To_StyleListAst<[U]>
-    : U extends StyleRuleBodyBuilder ? BodyBuilder_To_StyleRuleBody<[U]>
-    : never)[]
-  : [];
 
 export type UtilityConstruct<
   N extends string,
@@ -34,7 +21,7 @@ export type UtilityConstruct<
   $construct: "utility";
   name: N;
   theme?: T;
-  styles: BodyBuilder_To_UtilityBody<B>;
+  styles: (StyleListAst_WithMetadata<B> | StyleRuleAst_WithMetadata<B>)[];
 };
 
 export function utility<N extends string, B extends UtilityBodyBuilder[]>(
@@ -63,33 +50,46 @@ export function utility<N extends string>(
         []
       : [themeOrBody as StyleListBuilder | StyleRuleBodyBuilder]),
       ...body,
-    ].flatMap((b): (TailwindClassAst | StyleListAst | StyleRuleAst)[] => {
-      if (typeof b === "object" && b !== null && "$ast" in b) {
-        return [b as TailwindClassAst | StyleListAst | StyleRuleAst];
-      }
+    ].flatMap(
+      (
+        b,
+      ): (
+        | TailwindClassAst<TailwindUtility>
+        | StyleListAst
+        | StyleRuleAst
+      )[] => {
+        if (typeof b === "object" && b !== null && "$ast" in b) {
+          return [
+            b as
+              | TailwindClassAst<TailwindUtility>
+              | StyleListAst
+              | StyleRuleAst,
+          ];
+        }
 
-      if (
-        typeof b === "string" ||
-        (typeof b === "object" && b !== null && "prefix" in b)
-      ) {
-        return [dsl.tw(b)];
-      }
+        if (
+          typeof b === "string" ||
+          (typeof b === "object" && b !== null && "prefix" in b)
+        ) {
+          return [dsl.tw(b)];
+        }
 
-      const { $ = {}, ...rest } = b as dsl.StyleProperties & {
-        $?: Partial<Record<dsl.Selector, dsl.StyleListAst>>;
-      };
+        const { $ = {}, ...rest } = b as dsl.StyleProperties & {
+          $?: Partial<Record<dsl.Selector, dsl.StyleListAst>>;
+        };
 
-      return [
-        ...(Object.keys(rest).length > 0 ?
-          [dsl.styleList(rest as dsl.StyleProperties)]
-        : []),
-        ...Object.entries($).map(([selector, styles]) =>
-          dsl.styleRule(
-            selector as dsl.Selector,
-            ...(styles instanceof Array ? styles : [styles]),
+        return [
+          ...(Object.keys(rest).length > 0 ?
+            [dsl.styleList(rest as dsl.StyleProperties)]
+          : []),
+          ...Object.entries($).map(([selector, styles]) =>
+            dsl.styleRule(
+              selector as dsl.Selector,
+              ...(styles instanceof Array ? styles : [styles]),
+            ),
           ),
-        ),
-      ];
-    }),
+        ];
+      },
+    ),
   } as UtilityConstruct<N, any, any>;
 }
