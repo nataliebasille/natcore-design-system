28bcf9b61cf808a96dffa9e172f8b80df105c293
Natalie Basille <natalie.basille1@gmail.com>
2026-04-28 20:16:28 -0400

    Update sidebar

--- showcase-v2/app/_ui/sidebar/sidebar-content.tsx
+++ showcase-v2/app/_ui/sidebar/sidebar-content.tsx
@@ -10,7 +10,7 @@ export type SidebarProps = PropsWithChildren<{
 
 export function Sidebar({ className, children }: SidebarProps) {
   return (
-    <aside className={twMerge("p-4 max-tablet:tray-left", className)}>
+    <aside className={twMerge("p-2 px-3 max-tablet:tray-left", className)}>
       <SidebarToggle />
       {children}
     </aside>
@@ -25,7 +25,7 @@ export type SidebarGroupProps = PropsWithChildren<{
 export function SidebarGroup({ header, children }: SidebarGroupProps) {
   return (
     <div className="flex flex-col">
-      <div className="border-l-scale-2 mb-2 border-transparent pt-4 pr-2 pb-0 pl-3 text-xs tracking-wider text-tone-800-surface uppercase">
+      <div className="mb-2 border-transparent pt-4 pb-0 text-[.675rem] tracking-[.2em] text-(--tone-current-fg)/50 uppercase">
         {header}
       </div>
 
@@ -45,7 +45,7 @@ export function SidebarLink({ href, children }: SidebarLinkProps) {
     <NavLink
       href={href}
       component="li"
-      className="rounded-none! border-l-4 border-transparent p-2 pl-2 text-sm font-bold tracking-wide hover:cursor-pointer hover:border-l-tone-500-primary/50 hover:bg-tone-100-primary/10 active:border-l-tone-500-accent active:bg-tone-100-primary/10 active:text-tone-500-accent active:hover:border-l-tone-500-accent"
+      className="cursor-pointer rounded-lg border-l-2 border-transparent p-2 pl-2 text-sm tracking-wider text-(--tone-current-fg)/65 hover:bg-tone-200-surface hover:text-on-tone-200-surface active:rounded-l-none active:border-l-tone-500-accent active:bg-tone-200-surface active:text-on-tone-200-surface"
     >
       {children}
     </NavLink>
